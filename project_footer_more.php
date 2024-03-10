ae6bdc567aee737b6263ec02ddb28079fa63ffe8
Rohit Kumar <153207203+rohitkumar6207@users.noreply.github.com>
2024-03-11 00:00:01 +0530

    Add files via upload
    added project_footer_more.php

---
+++ project_footer_more.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Streamify_footer_more</title>
+    <link rel="stylesheet" href="project.css">
+    <style>
+        h2 ,p {
+            color: white;
+        }
+    </style>
+</head>
+<body style="background-color: black;">
+    <footer class="more">
+        <div class="footer_more_container">
+
+          <div class="footer-section">
+            <h3>About</h3><br>
+            <p>Streamify is your ultimate destination for streaming your favorite movies and TV shows.<a href="project_more.php">read more...</a></p>
+          </div>
+
+          <div class="footer-section">
+            <h3>Support</h3><br>
+            <!-- <p>Email: support@streamify.com</p>
+            <p>Phone: +123-456-7890</p> -->
+
+            <a href="">Help Center</a>
+            <a href="">Terms Of Use</a>
+            <a href="">Privacy Policy</a>
+            <a href="">Content Complaints</a>
+          </div>
+
+          <div class="footer-section">
+            <h3>Connect with Us</h3><br>
+            <p>Email: streamifycontact@gmail.com</p>
+            <p>Phone: 6207322327</p>
+            <p>Follow us on social media: @streamify</p>
+          </div>
+
+        </div>
+      </footer>
+</body>
+</html>
