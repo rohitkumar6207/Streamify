ea84577acf9e0d93f17122e3c9eb58825cb6d9e5
Rohit Kumar <153207203+rohitkumar6207@users.noreply.github.com>
2024-03-10 23:59:29 +0530

    Add files via upload
    added project_footer.php

---
+++ project_footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Streamify_footer</title>
+
+    <link rel="stylesheet" href="project.css">
+</head>
+<style>
+  p{
+    color: white;
+  }
+</style>
+<body background="dark-anime-pictures-iwmu3b0sun9r6789.jpg">
+    <footer class="copyright">
+        <div class="footer_container">
+          <p>&copy; 2024 Streamify. All rights reserved </p>
+        </div>
+        <div class="footer_container_image">
+          <img src="Screenshot_2024-02-17_010650-removebg.png" alt="logo" height="80px">
+        </div>
+      </footer>
+</body>
+</html>
