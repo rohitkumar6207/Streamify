d3d90c2fd126aeb01680762b66bb685cfcc94e57
Rohit Kumar <153207203+rohitkumar6207@users.noreply.github.com>
2024-03-11 00:06:23 +0530

    Add files via upload
    added project_premium.php

---
+++ project_premium.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Premium Subscription</title>
+
+  <link rel="stylesheet" href="project.css">
+</head>
+<body background="Images\small-set-of-40-manga-style-collage-for-walls-300-gsm-thick-original-imagfb3ygz73hykm.webp">
+  <div class="container_premium">
+    <h1 style="color: #fff;">Choose Your Premium Subscription</h1>
+    <div class="plans">
+
+      <div class="plan">
+        <h3>Streamify VIP<br> (₹199)</h3>
+        <p>Watch unlimited AVM, EDITs.</p>
+        <p>Stream on Moblie only.</p>
+        <p>Watch in HD and 720p.</p>
+        <p>Exclusive Streamify Specials.</p>
+        <a href="project_payment_199.php"><button>Subscribe</button></a>
+      </div>
+
+      <div class="plan">
+        <h3>Streamify Premium<br> (₹499)</h3>
+        <p>All benefits of Streamify VIP.</p>
+        <p>Watch in HD and 1080p.</p>
+        <p>Stream on 4 devices simultaneously.</p>
+        <p>Multi-language support.</p>
+        <a href="project_payment_999.php"><button>Subscribe</button></a>
+      </div>
+
+      <div class="plan">
+        <h3>Streamify Premium+ <br> (₹999)</h3>
+        <p>All benefits of Streamify Premium.</p>
+        <p>Stream on 8 devices simultaneously.</p>
+        <p>Watch in Full HD and 4K.</p>
+        <p>Download and watch offline.</p>
+        <p>Ad-free experience.</p>
+        <p>Exclusive Premieres.</p>
+        <a href="project_payment_999.php"><button>Subscribe</button></a>
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
