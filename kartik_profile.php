39b5b0abc87fdbb11c3336e36ae88a746f0d3b93
Rohit Kumar <153207203+rohitkumar6207@users.noreply.github.com>
2024-03-10 23:58:30 +0530

    Add files via upload
    added kartik_profile.php

---
+++ kartik_profile.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kartik Profile</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <style>
+        body{
+            background-color: #fbe4d8;
+        }
+div.social{
+    display: block;
+    align-items: center;
+    padding-top: 25px;
+    padding-left: 35px;
+    padding-bottom: 20px;
+    word-spacing: 18px;
+    width: 100%;
+}
+i{
+    color: black;
+}
+
+        div.container_header{
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    align-items: center;
+
+}
+header{
+    background-color: cornsilk;
+    border-radius: 18px;
+}
+
+header img{
+    border-radius: 18px;
+}
+p{
+    font-size: large;
+}
+
+.item {
+    width: 250px;
+    margin-right: 50px;
+    margin-left: 50px;
+    margin-bottom: 20px;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+    display: inline-block;
+}
+
+.item img {
+    width: 100%;
+    border-radius: 15px;
+}
+
+.item h3, h5 {
+    margin-top: 10px;
+    font-size: 18px;
+}
+
+.watch-button {
+    display: block;
+    margin-top: 10px;
+    background-color: #e50914;
+    color: #fff;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 5px;
+    text-align: center;
+    text-decoration: none;
+}
+.watch-button:hover {
+    background-color: #ff0000;
+}
+.h1{
+    text-align: center;
+}
+
+    </style>
+</head>
+<body>
+
+<header>
+    <div class="container_header">
+
+        <div class="main">
+                <img src="WhatsApp Image 2024-03-08 at 17.37.06_91b93466.jpg" height="280px" alt="Mypic" class="img-w">
+            </div>
+
+            <div class="detail">
+                <h3>Hi, I'm</h3>
+                <h1><span style="color:#f9532d;">Karkit</span> Parsad</h1> 
+                <p>I'm a Student of MCKV Institute Of Engineering. I am from bandel (West Bengal).
+                I am 22 year old, MY hobbies are Drawing, Football, Singing and Acting 
+                </p>  
+
+                <div class="social">
+                    <a href="https://www.facebook.com/share/jYj1CwR6kRZAPHBg/?mibextid=qi2Omg" target="_blank"><i class="fa-brands fa-facebook fa-bounce fa-xl"></i></a>
+
+                    <a href="https://www.instagram.com/r_kartik4u_?igsh=ZmdrZG5yODUwZ3Bj" target="_blank"><i class="fa-brands fa-instagram fa-bounce fa-xl"></i></i></i></a>
+
+                    <a href="" target="_blank"><i class="fa-brands fa-twitter fa-bounce fa-xl"></i></i></a>
+
+                    <a href="https://youtube.com/@hellokartik7393?si=m4eJpfxpob-rTmF7" target="_blank"><i class="fa-brands fa-youtube fa-bounce fa-xl"></i></i></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</header>
+
+<main>
+    <hr>
+<div class="container_body">
+<h1 class="h1">Content</h1>
+
+<div class="all-shows">
+
+    <div class="item">
+        <img src="hqdefault (2).webp" alt="">
+        <h3>//Kismat he kharaab hai//🥺🤦‍♂️</h3>
+        <a href="project_play3.php" class="watch-button">Watch</a>
+    </div>
+
+    <div class="item">
+        <img src="WhatsApp Image 2024-03-10 at 17.01.44_865c92a1.jpg" alt="">
+        <h3>//PAPA ka khauf//🥰</h3>
+        <a href="project_play4.php" class="watch-button">Watch</a>
+    </div>
+
+    <div class="item">
+        <img src="WhatsApp Image 2024-03-10 at 17.04.20_24029b2e.jpg" alt="">
+        <h3>Reunited Friendship</h3>
+        <a href="project_play5.php" class="watch-button">Watch</a>
+    </div>
+
+</div>
+</main>
+
+</body>
+</html>
