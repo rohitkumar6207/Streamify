0f0c88238a1aa3a39f43fefd2c3b4d3787b094f7
Rohit Kumar <153207203+rohitkumar6207@users.noreply.github.com>
2024-03-10 23:58:04 +0530

    Add files via upload
    added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+</head>
+<body background="dark-anime-pictures-iwmu3b0sun9r6789.jpg">
+    <?php
+    include'project_nav.php';
+
+    include'project_main.php';
+
+    include'project_footer_more.php';
+
+    include'project_footer.php';
+    ?>
+</body>
+</html>
